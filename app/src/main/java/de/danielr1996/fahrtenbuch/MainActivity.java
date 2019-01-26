fc988894a2e027fa9cc78a0736dd677a32fd91a4
Daniel Richter <danielrichter@posteo.de>
2019-01-26 01:28:34 +0100

    Initial Commit

---
+++ app/src/main/java/de/danielr1996/fahrtenbuch/MainActivity.java
@@ -0,0 +1,14 @@
+package de.danielr1996.fahrtenbuch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
