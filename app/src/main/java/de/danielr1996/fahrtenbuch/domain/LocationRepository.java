e0506b7be80f4a2b2561a9c4fb068de7a2b2a1f0
Daniel Richter <danielrichter@posteo.de>
2019-01-30 20:13:37 +0100

    refactor(#5): Refactoring - Remove start/stop methods from PlayServiceLocationRepository

--- app/src/main/java/de/danielr1996/fahrtenbuch/domain/LocationRepository.java
+++ app/src/main/java/de/danielr1996/fahrtenbuch/domain/LocationRepository.java
@@ -2,9 +2,9 @@ package de.danielr1996.fahrtenbuch.domain;
 
 import java.util.List;
 
-import io.reactivex.Flowable;
+import io.reactivex.Observable;
 
 public interface LocationRepository {
-    Flowable<Messung> getMessung();
-    Flowable<List<Messung>> getMessungen();
+    Observable<Messung> getMessung();
+    Observable<List<Messung>> getMessungen();
 }
